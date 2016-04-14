af341427ef9997b284694d0117040f0152c3e994
Thomas Moffet <pantherman1996@gmail.com>
2016-04-14 16:56:16 +1000

    Added some Eclipse project files so that you can just import the project directly.

---
+++ src/MazeProgram.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author Group 2
+ *
+ */
+public class MazeProgram {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		System.out.println("Our program does a lot of things.");
+	}
+
+}
